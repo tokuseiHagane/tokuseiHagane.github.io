900f450d6103c72d3368084aa4c844d8f4092bf7
KonstantinBiaCo <kbc.off@list.ru>
2021-12-06 15:48:18 +0300

    rev_1.1

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ function ToggleMenu(){
     const button = document.getElementById('burger')
     const menu = document.getElementById('menu-burger')
 
-
     button.classList.toggle('active')
     menu.classList.toggle('active')
-    
 }
